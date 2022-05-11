406ffa1104af135d64435af9389ec23a3b13edbe
Ashish Shukla <ashishshukla6161@gmail.com>
2022-05-11 18:36:07 +0530

    Set app for Heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -159,7 +160,7 @@ app.get('*', (req, res)=>{
 
 
 // Start the server
-app.listen(3000, ()=>{
-    console.log('Server started on port 3000')
+app.listen(port, ()=>{
+    console.log('Server started on port ' + port)
 }) // Start the server on port 3000 can be accesses using localhost:3000 for local environment
 
