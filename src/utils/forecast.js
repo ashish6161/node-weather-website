1944680530049d2df7c665d94b232e93f342d89e
Ashish Shukla <ashishshukla6161@gmail.com>
2022-05-12 19:27:39 +0530

    changes in forecast string displayed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -26,7 +26,7 @@ postman({ url, json: true }, (error, { body }) => {
     } else if (body.error) {
         callback('Unable to find location', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. Have a great day!')
     }
 })
 }
